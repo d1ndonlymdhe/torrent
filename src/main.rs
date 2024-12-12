6b7f472f68464d5e3d6404a8debbcfe7f68b5b71
Ayush Shrestha <ayushshrestha8920@protonmail.com>
2024-12-12 09:01:43 +0545

    simple bencode parsing, doesn't work with binary blobs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
+use std::fs::read_to_string;
+use std::ptr::read_volatile;
+use crate::bencode::parse_bencode;
+
 mod bencode;
 mod str_utils;
 
 fn main() {
-    println!("Hello, world!");
+    let content = read_to_string("test.torrent").unwrap();
+    
+    println!("{:#?}", parse_bencode(content));
 }
